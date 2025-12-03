36bc30efc77ae21bf3c360df96f431278a3e8d20
robhitchens <>
2025-12-03 09:28:46 -0500

    some cleanup

--- day1/solution.sh
+++ day1/solution.sh
@@ -1,24 +1,41 @@
 #!/usr/bin/env bash
 
-function main {
+declare -g debug
+
+function setOptions {
 	while getopts 'd' opt; do
 		case "$opt" in
-		d) local debug=1 ;;
-		*) local debug=0 ;;
+		d) debug=1 ;;
+		*) debug=0 ;;
 		esac
 	done
-	local inputFile="$1"
+}
+
+function log {
+	if [[ "$debug" == '1' ]]; then
+		echo "$1"
+	fi
+}
+
+function main {
+	setOptions "$@"
+	for item in $@; do
+		if [[ -f "$item" ]]; then
+			local inputFile="$item"
+			break 2
+		fi
+	done
 	local zeroCount=0
-	local currentPosition=50
+	local dial=50
 	while IFS=$'\n' read -r line; do
 		if [[ -z $line ]]; then
 			continue
 		fi
-		#	local pos=(${line/\v(L|R)([0-9]+)/\1\ \2/})
-		if ((debug == 1)); then echo "cur  : $currentPosition"; fi
+		log "cur  : $dial"
 
+		#	local pos=(${line/\v(L|R)([0-9]+)/\1\ \2/})
 		local pos=($(echo "$line" | sed -E 's/(L|R)([0-9]+)/\1 \2/'))
-		if ((debug == 1)); then echo "input: ${pos[@]}"; fi
+		log "input: ${pos[@]}"
 
 		if [[ "${pos[0]}" == 'L' ]]; then
 			local sign=-1
@@ -27,18 +44,19 @@ function main {
 		fi
 
 		local rem=$((${pos[1]} % 100))
-		((currentPosition += rem * sign))
-		if ((debug == 1)); then echo "adjus: $currentPosition"; fi
-		if ((currentPosition < 0)); then
-			if ((debug == 1)); then echo "readj: 100 + $currentPosition"; fi
-			((currentPosition += 100))
-		elif ((currentPosition > 99)); then
-			if ((debug == 1)); then echo "readj: $currentPosition - 100"; fi
-			((currentPosition -= 100))
+		((dial += rem * sign))
+		log "adjus: $dial"
+		if ((dial < 0)); then
+			log "readj: 100 + $dial"
+			((dial += 100))
+		elif ((dial > 99)); then
+			log "readj: $dial - 100"
+			((dial -= 100))
 		fi
-		if ((debug == 1)); then echo "end  : $currentPosition"; fi
+		log "end  : $dial
+"
 
-		if ((currentPosition == 0)); then
+		if ((dial == 0)); then
 			((zeroCount += 1))
 		fi
 	done <$inputFile
