01b7f088b09ad42d02d255adc97a3a02b2be9f67
Rob Hitchens <rjhitchens3@gmail.com>
2025-12-09 22:32:30 -0500

    small refactor, started working on part2 solution

--- day1/solution.sh
+++ day1/solution.sh
@@ -1,32 +1,42 @@
 #!/usr/bin/env bash
 
-declare -g debug
+declare -g debug=0 p1=0 p2=0
 
 function setOptions {
 	while getopts 'd' opt; do
 		case "$opt" in
 		d) debug=1 ;;
-		*) debug=0 ;;
+		esac
+	done
+	for flag in $@; do
+		case ${flag/-/} in
+		p1) p1=1 ;;
+		p2) p2=1 ;;
 		esac
 	done
 }
 
 function log {
-	if [[ "$debug" == '1' ]]; then
-		echo "$1"
+	if ((debug == 1)); then
+		echo "$1" >&2
 	fi
 }
 
-function main {
-	setOptions "$@"
+function setupInitialState {
 	for item in $@; do
 		if [[ -f "$item" ]]; then
 			local inputFile="$item"
 			break 2
 		fi
 	done
-	local zeroCount=0
-	local dial=50
+	echo "$inputFile 0 50"
+}
+
+function part1 {
+	local state=($(setupInitialState "$@"))
+	local inputFile="${state[0]}"
+	local zeroCount="${state[1]}"
+	local dial="${state[2]}"
 	while IFS=$'\n' read -r line; do
 		if [[ -z $line ]]; then
 			continue
@@ -64,6 +74,72 @@ function main {
 		fi
 	done <$inputFile
 	echo "$zeroCount"
+}
+
+function part2 {
+	local state=($(setupInitialState "$@"))
+	local inputFile="${state[0]}"
+	local zeroCount="${state[1]}"
+	local dial="${state[2]}"
+	while IFS=$'\n' read -r line; do
+		if [[ -z $line ]]; then
+			continue
+		fi
+		log "cur  : $dial"
+		if [[ $line =~ (L|R)([0-9]+) ]]; then
+			local pos=(${BASH_REMATCH[@]:1})
+			log "pos v: ${pos[@]}"
+		else
+			log "$line didn't match expression"
+		fi
+		log "input: ${pos[@]}"
+
+		if [[ "${pos[0]}" == 'L' ]]; then
+			local sign=-1
+		elif [[ "${pos[0]}" == 'R' ]]; then
+			local sign=1
+		fi
+
+		local rem=$((${pos[1]} % 100))
+		local div=$((${pos[1]} / 100))
+		local additionalClicks=0
+		((dial += rem * sign))
+		log "adjus: $dial"
+		if ((dial < 0)); then
+			log "readj: 100 + $dial"
+			((dial += 100))
+			((additionalClicks++))
+		elif ((dial > 99)); then
+			log "readj: $dial - 100"
+			((dial -= 100))
+			((additionalClicks++))
+		fi
+		if ((div > 0)); then
+			((additionalClicks *= div))
+		fi
+
+		log "addCl: $additionalClicks"
+		if ((additionalClicks > 0)); then
+			((zeroCount += additionalClicks))
+		fi
+		log "end  : $dial
+"
+
+		if ((dial == 0)); then
+			((zeroCount += 1))
+		fi
+
+	done <$inputFile
+	echo "$zeroCount"
+}
+
+function main {
+	setOptions "$@"
+	if ((p1 == 1)); then
+		part1 "$@"
+	elif ((p2 == 1)); then
+		part2 "$@"
+	fi
 }
 
 main "$@"
--- day1/solution.test.sh
+++ day1/solution.test.sh
@@ -2,10 +2,22 @@
 
 source bsunit-lib.sh
 
+#SETUP
+function setup {
+	#set -x
+	return 0
+}
+
+#TEARDOWN
+function teardown {
+	#set +x
+	return 0
+}
+
 #TEST
 function part1_test_usingExampleInput {
 	# Assuming running at repo parent
-	local output="$(./day1/solution.sh ./day1/p1-example.txt)"
+	local output="$(./day1/solution.sh p1 ./day1/p1-example.txt)"
 
 	assert "$output" equals "3"
 }
@@ -13,7 +25,24 @@ function part1_test_usingExampleInput {
 #TEST
 function part1_test_usingRealInput {
 	# Assuming running at repo parent
-	local output="$(./day1/solution.sh ./day1/p1-input.txt)"
+	local output="$(./day1/solution.sh p1 ./day1/p1-input.txt)"
 
 	assert "$output" equals "1118"
+}
+
+#TEST
+function part2_test_usingExampleInput {
+	# Assuming running at repo parent
+	local output="$(./day1/solution.sh -d p2 ./day1/p1-example.txt)"
+
+	assert "$output" equals "6"
+}
+
+#TEST
+function part2_test_usingRealInput {
+	# Assuming running at repo parent
+	local output="$(./day1/solution.sh p2 ./day1/p1-input.txt)"
+
+	# FIXME: figure out what the actual answer is.
+	assert "$output" equals "0"
 }
